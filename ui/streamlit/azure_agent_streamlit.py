9d4b7a97db8e4a72f0181fb2e1e984d72088ef5f
Panturat <jaruwat.panturat@allianz.com>
2024-08-10 13:48:02 +0700

    feat: text to speech added

--- ui/streamlit/azure_agent_streamlit.py
+++ ui/streamlit/azure_agent_streamlit.py
@@ -2,6 +2,26 @@ import streamlit as st
 import random
 import time
 from audiorecorder import audiorecorder
+import azure.cognitiveservices.speech as speechsdk
+
+speech_key = ''
+service_region = '' 
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+speech_config.speech_synthesis_voice_name = "en-US-BrianMultilingualNeural"
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+def textToSpeech(text):
+    result = speech_synthesizer.speak_text_async(text).get()
+    # Check result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized for text [{}]".format(text))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+
+
 
 # Streamed response emulator
 def response_generator():
@@ -12,6 +32,8 @@ def response_generator():
             "Do you need help?",
         ]
     )
+    
+    textToSpeech(response)
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
@@ -38,7 +60,8 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
+        chatResponse = response_generator()
+        response = st.write_stream(chatResponse)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
@@ -53,3 +76,4 @@ if len(audio) > 0:
 
     # To get audio properties, use pydub AudioSegment properties:
     st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds") 
+
