a6b3489d0f6e4c2f75e718ca182ffc101eca194c
Chanchai Itthiamornlerd <chanchai.itthiamornlerd@allianz.com>
2024-08-11 15:09:16 +0700

    feat: add genai

--- ui/streamlit/azure_agent_streamlit.py
+++ ui/streamlit/azure_agent_streamlit.py
@@ -1,14 +1,34 @@
+import os
 import streamlit as st
 import random
 import time
 from audiorecorder import audiorecorder
 import azure.cognitiveservices.speech as speechsdk
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
-speech_key = ''
-service_region = '' 
+speech_key = os.getenv("AZURE_SPEECH_KEY", "")
+service_region = os.getenv("AZURE_SERVICE_REGION", "")
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-speech_config.speech_synthesis_voice_name = "en-US-BrianMultilingualNeural"
+speech_config.speech_synthesis_voice_name = "en-US-AmberNeural"
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+endpoint = os.getenv("AZURE_AI_ENDPOINT_URL", "")
+deployment = os.getenv("AZURE_AI_DEPLOYMENT_NAME", "")
+search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT", "")
+search_key = os.getenv("AZURE_SEARCH_KEY", "")
+embedding_endpoint = os.getenv("AZURE_AI_EMBEDDING_ENDPOINT", "")
+embedding_key = os.getenv("AZURE_EMBEDDING_KEY", "")
+
+token_provider = get_bearer_token_provider(
+    DefaultAzureCredential(),
+    "https://cognitiveservices.azure.com/.default")
+
+client = AzureOpenAI(
+    azure_endpoint=endpoint,
+    azure_ad_token_provider=token_provider,
+    api_version="2024-05-01-preview",
+)
 
 def textToSpeech(text):
     result = speech_synthesizer.speak_text_async(text).get()
@@ -24,16 +44,53 @@ def textToSpeech(text):
 
 
 # Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
+def response_generator(prompt):
+    completion = client.chat.completions.create(
+        model=deployment,
+        messages= [
+        {
+          "role": "user",
+          "content": f"{prompt}"
+        }],
+        max_tokens=800,
+        temperature=0.7,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+        stream=False,
+        extra_body={
+          "data_sources": [{
+              "type": "azure_search",
+              "parameters": {
+                "filter": None,
+                "endpoint": f"{search_endpoint}",
+                "index_name": "ai-build-techday-v1",
+                "semantic_configuration": "azureml-default",
+                "authentication": {
+                  "type": "api_key",
+                  "key": f"{search_key}"
+                },
+                "embedding_dependency": {
+                  "type": "endpoint",
+                  "endpoint": f"{embedding_endpoint}",
+                  "authentication": {
+                    "type": "api_key",
+                    "key": f"{embedding_key}"
+                  }
+                },
+                "query_type": "vector_simple_hybrid",
+                "in_scope": True,
+                "role_information": "You are an AI assistant that helps people find information named Ava working for ABCompany. ",
+                "strictness": 3,
+                "top_n_documents": 5
+              }
+            }]
+        }
     )
-    
-    textToSpeech(response)
+    print(completion.to_json())
+    response = completion.choices[0].message.content
+
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
@@ -60,8 +117,9 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        chatResponse = response_generator()
+        chatResponse = response_generator(prompt)
         response = st.write_stream(chatResponse)
+    textToSpeech(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
